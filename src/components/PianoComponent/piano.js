c85bdfd50db087fd726de56b1e20bebf9c7cf6df
LIBERCOSOFT <akinspreciousola@gmail.com>
2023-04-08 16:17:51 +0100

    Rewrite piano.js file

--- src/components/PianoComponent/piano.js
+++ src/components/PianoComponent/piano.js
@@ -1,138 +1,199 @@
-import React from "react";
+import React from 'react';
 
 class Piano extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state ={
-            key: ''
-        }
+    this.state = {
+      key: '',
+    };
+  }
 
+  pianoButtonsClick = (e) => {
+    const { id } = e.target;
+    let audio = document.getElementById(id.split('-')[0]);
+    audio.currentTime = 0;
+    audio.play();
+    this.setState({
+      key: audio.className,
+    });
+  };
 
-    this.clickQ = this.clickQ.bind(this);
-    this.clickW = this.clickW.bind(this);
-    this.clickE = this.clickE.bind(this);
-    this.clickA = this.clickA.bind(this);
-    this.clickS = this.clickS.bind(this);
-    this.clickD = this.clickD.bind(this);
-    this.clickZ = this.clickZ.bind(this);
-    this.clickX = this.clickX.bind(this);
-    this.clickC = this.clickC.bind(this);
-    }
-
-/* buttons Onclick function to play the audio assocaited with them through their id and set the state with
-    their name  */
-    clickQ(){
-        document.getElementById('q').play();
-        this.setState({
-            key: 'Chord 1'
-        });
-    }
-
-    clickW(){
-        document.getElementById('w').play();
-        this.setState({
-            key: 'Chord 2'
-        });
-    }
-
-    clickE(){
-        document.getElementById('e').play();
-        this.setState({
-            key: 'Chord 3'
-        });
-    }
-    clickA(){
-        document.getElementById('a').play();
-        this.setState({
-            key: 'Shaker'
-        });
-    }
-    clickS(){
-        document.getElementById('s').play();
-        this.setState({
-            key: 'Open HH'
-        });
-    }
-    clickD(){
-        document.getElementById('d').play();
-        this.setState({
-            key: 'Closed HH'
-        });
-    }
-    clickZ(){
-        document.getElementById('z').play();
-        this.setState({
-            key: 'Punchy-Kick'
-        });
-    }
-    clickX(){
-        document.getElementById('x').play();
-        this.setState({
-            key: 'Side-Stick'
-        });
-    }
-    clickC(){
-        document.getElementById('c').play();
-        this.setState({
-            key: 'Snare'
-        });
-    }
-    render(){
-        return(
-        <div>
+  render() {
+    return (
+      <div>
         <h1>Piano Machine</h1>
 
         <div>
-            <audio id="q"> <source src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3" type="audio/mp3" /> </audio>
-            <audio id="w"> <source src="https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3" type="audio/mp3" /> </audio>
-            <audio id="e"> <source src="https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3" type="audio/mp3" /> </audio>
-            <audio id="a"> <source src="https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3" type="audio/mp3" /> </audio>
-            <audio id="s"> <source src="https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3" type="audio/mp3" /> </audio>
-            <audio id="d"> <source src="https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3" type="audio/mp3" /> </audio>
-            <audio id="z"> <source src="https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3" type="audio/mp3" /> </audio>
-            <audio id="x"> <source src="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3" type="audio/mp3" /> </audio>
-            <audio id="c"> <source src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" type="audio/mp3" /> </audio>
+          <audio id="q" className='Chord 1'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="w" className='Chord 2'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="e" className='Chord 3'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="a" className='Shaker'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="s" className='Open HH'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="d" className='Closed HH'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="z" className='Punchy-Kick'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="x" className='Side-Stick'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
+          <audio id="c" className='Snare'>
+            {' '}
+            <source
+              src="https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
+              type="audio/mp3"
+            />{' '}
+          </audio>
         </div>
 
         <div className="home-container">
           <div className="grid-container">
-            <button className="grid-item" onClick={this.clickQ}>Q</button>
-            <button className="grid-item" onClick={this.clickW}>W</button>
-            <button className="grid-item" onClick={this.clickE}>E</button>
-            <button className="grid-item" onClick={this.clickA}>A</button>
-            <button className="grid-item" onClick={this.clickS}>S</button>
-            <button className="grid-item" onClick={this.clickD}>D</button>
-            <button className="grid-item" onClick={this.clickZ}>Z</button>
-            <button className="grid-item" onClick={this.clickX}>X</button>
-            <button className="grid-item" onClick={this.clickC}>C</button>
+            <button
+              type='button'
+              className="grid-item"
+              id="q-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              Q
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="w-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              W
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="e-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              E
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="a-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              A
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="s-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              S
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="d-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              D
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="z-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              Z
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="x-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              X
+            </button>
+            <button
+              type='button'
+              className="grid-item"
+              id="c-btn"
+              onClick={this.pianoButtonsClick}
+            >
+              C
+            </button>
           </div>
-
           <div className="right-container">
-          <h5>Power</h5>
+            <h5>Power</h5>
             <label className="switch">
-                <input type="checkbox" onClick={this.props.powerOff} checked/>
-                <span className="slider" />
+              <input type="checkbox" onClick={this.props.powerOff} checked />
+              <span className="slider" />
             </label>
-            
+
             <div className="key-container">
-                <h4> {this.state.key} </h4>
+              <h4> {this.state.key} </h4>
             </div>
 
             <h5>Bank</h5>
             <label className="switch">
-                <input type="checkbox" onClick={this.props.pianoBankClick} checked/>
-                <span className="slider" />
+              <input
+                type="checkbox"
+                onClick={this.props.pianoBankClick}
+                checked
+              />
+              <span className="slider" />
             </label>
           </div>
-
         </div>
 
-        <p>coded by <a href="https://github.com/LIBERCOSOFT">LIBERCOSOFT</a></p>
+        <p>
+          coded by <a href="https://github.com/LIBERCOSOFT">LIBERCOSOFT</a>
+        </p>
       </div>
     );
-    }
+  }
 }
 
-
-export default Piano
+export default Piano;
